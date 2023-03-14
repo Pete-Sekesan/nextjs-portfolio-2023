ec06e58d9e6c0c8a523a5f8152cd256e1a9fc7e7
Peter Sekesan <peter.sekesan@newsday.com>
2023-03-14 17:01:31 -0400

    added nav mobile menu divs and content

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
       <div className='fixed w-full h-20 shadow-xl z-[100]'>
           <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
               <Image
-                 src="/../public/assets/Peter Sekesan (1).png"
+                 src="/../public/assets/ps-logo-orange.png"
                  alt="/"
                  width='125'
                  height='20' />
@@ -29,10 +30,27 @@ const Navbar = () => {
                   <Link href='/'>
                       <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                   </Link>
-              </ul>
+                  </ul>
+                  <div className='md:hidden'>
+                      <AiOutlineMenu size={25} />
+                  </div>
           </div>
           </div>
-    </div>
+          <div className='fixed left-0 top-0 w-full h-screen  bg-black/70'>
+              <div className='fixed left-0 top-0 w-[75%] sm:w-60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+                  <div>
+                  <div className='flex w-full items-center justify-between'>
+                          <Image src='/../public/assets/ps-logo-orange.png' width='87' height='35' alt='/' />
+                          <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                      <AiOutlineClose/>
+                  </div>
+                  </div>
+                
+              </div>
+              
+          </div>
+          </div>
+          </div>
   )
 }
 
