fc3d6d53cd1055c7ff5efe59c132b6dbaf351c50
Peter Sekesan <peter.sekesan@newsday.com>
2023-03-16 12:27:21 -0400

    Main component built out

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
 function Main() {
   return (
@@ -8,10 +11,25 @@ function Main() {
                   <p className='uppercase text-sm tracking-widest text-gray-600'> Let's Build Something</p>   
                   <h1 className='py-4 text-gray-700'> Hi, I'm <span className='text-[#ff9933]' >Pete</span></h1>
                   <h1 className='py-2 text-gray-700'>A Full-Stack Developer</h1>
-                  <p>I am a Software Engineer currently working as a Frontend UX Developer for Adelphi University in Garden City, NY.
+                  <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I am a Software Engineer currently working as a Frontend UX Developer for Adelphi University in Garden City, NY.
                     I have a serious passion for building excellent software and web technologies that help improve the lives of their users.
                     I am interested in the entire full-stack spectrum and would love to work on ambitious Web Technologies, Sites and Products with positive people.  
-                    Whenever possible, I apply my passion for developing products with Node.js, PostgreSQL and Modern Javascript Library and Frameworks  like React.js, jQuery and Gatsby.js  and always looking to learn something new.</p>
+                      Whenever possible, I apply my passion for developing products with Node.js, PostgreSQL and Modern Javascript Library and Frameworks  like React.js, jQuery and Gatsby.js  and always looking to learn something new.
+                  </p>
+                  <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                          <FaLinkedinIn/>
+                      </div>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                          <FaGithub/>
+                      </div>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                          <AiOutlineMail/>
+                      </div>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                          <BsFillPersonLinesFill/>
+                      </div>
+                  </div>
               </div>
           </div>
     </div>
