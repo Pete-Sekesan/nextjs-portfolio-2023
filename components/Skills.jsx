22f728d89ca42b42a1f6d86dfaf253e4896146fa
Peter Sekesan <peter.sekesan@newsday.com>
2023-03-17 13:05:35 -0400

    Skills icons layout

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import {DiJavascript1} from 'react-icons/di'
 
 
@@ -12,9 +13,66 @@ function Skills() {
                   <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                       <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                           <div className='mx-auto'>
-                           
+                              <Image src='/../public/assets/ps-logo-orange.png'
+                                    alt='/'
+                                  width='64'
+                              height='64'/>
                           </div>
-                           
+                          <div className='flex flex-col items-center justify-center'>
+                              <h3>HTML</h3>
+                          </div>
+                      </div>
+                  </div> 
+                  <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                      <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                          <div className='mx-auto'>
+                              <Image src='/../public/assets/ps-logo-orange.png'
+                                    alt='/'
+                                  width='64'
+                              height='64'/>
+                          </div>
+                          <div className='flex flex-col items-center justify-center'>
+                              <h3>HTML</h3>
+                          </div>
+                      </div>
+                  </div> 
+                  <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                      <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                          <div className='mx-auto'>
+                              <Image src='/../public/assets/ps-logo-orange.png'
+                                    alt='/'
+                                  width='64'
+                              height='64'/>
+                          </div>
+                          <div className='flex flex-col items-center justify-center'>
+                              <h3>HTML</h3>
+                          </div>
+                      </div>
+                  </div> 
+                  <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                      <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                          <div className='mx-auto'>
+                              <Image src='/../public/assets/ps-logo-orange.png'
+                                    alt='/'
+                                  width='64'
+                              height='64'/>
+                          </div>
+                          <div className='flex flex-col items-center justify-center'>
+                              <h3>HTML</h3>
+                          </div>
+                      </div>
+                  </div> 
+                  <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                      <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                          <div className='mx-auto'>
+                              <Image src='/../public/assets/ps-logo-orange.png'
+                                    alt='/'
+                                  width='64'
+                              height='64'/>
+                          </div>
+                          <div className='flex flex-col items-center justify-center'>
+                              <h3>HTML</h3>
+                          </div>
                       </div>
                   </div> 
               </div>
