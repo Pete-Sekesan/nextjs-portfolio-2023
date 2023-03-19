25833b945e8acd2a22f782deb04ce37a1e0fc771
Peter Sekesan <peter.sekesan@newsday.com>
2023-03-19 14:44:51 -0400

    contact form div adjustments

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 function Contact() {
     return (
@@ -9,7 +12,7 @@ function Contact() {
                 <h2 className='py-4'>Get In Touch</h2>
                 <div className='grid lg:grid-cols-5 gap-8'>
                     <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl py-4'>
-                        <div className='lg:p-4 h-full roundd-xl'>
+                        <div className='lg:p-4 h-full round-xl'>
                             <div>
                                 <Image
                                     src="/../public/assets/PeteHero.JPG"
@@ -17,6 +20,29 @@ function Contact() {
                                     width='300'
                                     height='60'
                                     className='rounded-xl hover:scale-105 ease-in duration-300' />
+                            </div>
+                            <div>
+                                <h2 className='py-2'>Peter Sekesan</h2>
+                                <p>Full-Stack Developer</p>
+                                <p className='py-4'>I am available for freelance positions. Contact me and let's talk</p>
+                            </div>
+
+                            <div>
+                                <p className='uppercase pt-8'>Connect With Me</p>
+                                <div className='flex items-center justify-between py-4'>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                        <FaLinkedinIn />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                        <FaGithub />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                        <AiOutlineMail />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                        <BsFillPersonLinesFill />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
