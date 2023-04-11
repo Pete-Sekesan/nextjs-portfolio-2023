ffb42002c20d42aab93ade72cddf72e28792e37d
Peter Sekesan <peter.sekesan@newsday.com>
2023-04-11 09:00:43 -0400

    removed down page js

--- pages/download.js
+++
@@ -1,9 +0,0 @@
-import { join } from 'path';
-import { createReadStream } from 'fs';
-import { sendFile } from 'micro';
-
-export default async function handler(req, res) {
-    const filePath = join(process.cwd(), 'public', 'assets', 'myf.png');
-    const fileStream = createReadStream(filePath);
-    await sendFile(res, 200, null, fileStream);
-}
